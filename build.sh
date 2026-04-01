867c66333fef81a8f4a1ec8b8ce466487955818a
Adityabodhi <adityabodhigaikwad2004@gmail.com>
2026-04-01 16:26:20 +0530

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+pip install -r requirements.txt
+
+python manage.py collectstatic --no-input
+python manage.py migrate
+
+# Seed the database and images
+python setup.py
+python update_project_images.py
